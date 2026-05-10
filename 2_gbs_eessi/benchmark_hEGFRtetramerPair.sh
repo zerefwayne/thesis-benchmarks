aa6d7f207f3918911ddd182040481b39f6416581
Aayush Joglekar <aayushjog@gmail.com>
2026-05-10 20:06:01 +0300

    Add GROMACS benchmarking suite for EESSI

---
+++ 2_gbs_eessi/benchmark_hEGFRtetramerPair.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=gromacs_hegfr_tetramer_pair
+#SBATCH --account=project_462000226
+#SBATCH --partition=dev-g
+#SBATCH --nodes=1
+#SBATCH --gpus=8
+#SBATCH --exclusive
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=7
+#SBATCH --time=01:00:00
+#SBATCH --output=results/%x_%j.out
+#SBATCH --constraint=eessi
+
+source common.sh
+module load GROMACS/2025.1-rfoss-2025a-SYCL
+
+cd ./GROMACS_Benchmark_Suite/HECBioSim/hEGFRtetramerPair
+
+mpirun -np 8 --oversubscribe gmx_mpi mdrun \
+    -s benchmark.tpr \
+    -nb gpu -pme gpu -bonded gpu \
+    -nsteps 100000 -resetstep 20000 -noconfout -npme 1
